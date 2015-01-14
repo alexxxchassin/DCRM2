2730a83ddd5309058ee76fd1d221f32dd24ade34
Alex <ruby@champagnemail.com>
2015-01-14 17:26:13 -0500

    Profile page updates

--- frontend/dcrm.js
+++ frontend/dcrm.js
@@ -41,13 +41,13 @@ function timeConverter(unconvertedTime){
 function getUrlParameter(sParam)
 {
     var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
+    var sURLVariables = sPageURL.split('?'); 
     for (var i = 0; i < sURLVariables.length; i++) 
     {
         var sParameterName = sURLVariables[i].split('=');
         if (sParameterName[0] == sParam) 
         {
-            return sParameterName[1];
+            return sParameterName[1];   
         }
     }
 }          
@@ -109,6 +109,7 @@ $(document).on('pagebeforeshow', '#headline', function(){
 $("#profilelink").click(function(e) {
     e.preventDefault();
     window.location.href = $(this).attr("href") +'?' + "user=" + messageData.id;
+
 });
 });
 
@@ -148,35 +149,50 @@ $(document).on('vclick', '#contact-list li a', function(){
 });
 
 
+/* $(document).on('pageinit', '#profile', function(){      
+    var url = 'data1.json'
+    var userPassed = getUrlParameter("user");
+    console.log(userPassed + " top")     
+    timestamp = getLastTimestamp(messageData[userPassed])
+    console.log(messageData[userPassed])
+    //recentSender = getLastSender(messageData[userPassed])
+    console.log(messageData.userPassed)
+                if (timestamp === "45 years ago") {
+                    $('#commhist').append('No messages yet...');
+
+                }
+                else { 
+                $('#commhist').append('<li>The last message was sent '+ timestamp +' by ' + recentSender.toLowerCase() + ' </li>');
+            };
+$('#profileusername').append("<strong>" + userPassed + "</strong>");        
+            });
+
+*/
 $(document).on('pageinit', '#profile', function(){      
-    var url = 'data1.json'       
-    
-   /* $.ajax({
+    var url = 'data1.json'   
+    var userPassed = getUrlParameter("user");    
+    $('#profileusername').append("<strong>" + userPassed + "</strong>");
+
+    $.ajax({
         url: url,
         dataType: "json",
         async: true,
         success: function (result) {
             messageData.messages = result.messages;
             var timestamp = null;
-            var recentSender = null; 
-            $.each(result.messages, function(username, data){ */
-               var timestamp = getLastTimestamp(messageData.id);
-               var recentSender = getLastSender(messageData.id);
-                timestamp = timeConverter(timestamp);
-                if (timestamp === "45 years ago" && recentSender === messageData.id) {
+            var recentSender = null;
+                timestamp = getLastTimestamp(messageData.messages[userPassed]);
+                recentSender = getLastSender(messageData.messages[userPassed]);
+               timestamp = timeConverter(timestamp);
+                if (recentSender === null) {
                     $('#commhist').append('No messages yet...');
-                    console.log("if" + timestamp + recentSender + userNameClicked);
-
                 }
-                else if (recentSender === messageData.id && timestamp !== "45 years ago") {
-                    console.log("elseif" + timestamp + recentSender + userNameClicked);
-                   
-
-                $('#commhist').append('<li>The last message was sent '+ timestamp +' by ' + recentSender.toLowerCase() +' </li>');
-
-            }
-            
-var userPassed = getUrlParameter(user);
-$('#profileusername').append(userPassed);
-           
-            });
+                else {
+                $('#commhist').append("The last message was sent " + timestamp + " by "  + recentSender );
+                    }
+            },
+        error: function (request,error) {
+            alert('Network error has occurred please try again!');        
+        }
+});
+});
